2044a5a844618b29ff75c210038c6f280ce4a43a
AdrianArnoso <57632143+AdrianArnoso@users.noreply.github.com>
2019-12-04 18:05:54 +0100

    isAbsolute mal

--- explorer/src/app/shared/shared.module.ts
+++ explorer/src/app/shared/shared.module.ts
@@ -1,8 +1,8 @@
-import { CommonModule } from "@angular/common";
-import { NgModule } from "@angular/core";
-import { MatListModule } from "@angular/material/list";
-import { RouterModule } from "@angular/router";
-import { CountriesListComponent } from "./countries-list/countries-list.component";
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
+import { RouterModule } from '@angular/router';
+import { CountriesListComponent } from './countries-list/countries-list.component';
 import { RegionsListComponent } from './regions-list/regions-list.component';
 
 @NgModule({
